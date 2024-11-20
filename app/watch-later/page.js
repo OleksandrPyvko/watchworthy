badbe300de9b54ef80a18ff9ccf65397b2ec652c
Oleksandr Pyvko <pyvko.a.g@gmail.com>
2024-11-20 16:01:21 +0200

    adding to wath later list / rendering list

--- app/watch-later/page.js
+++ app/watch-later/page.js
@@ -1,11 +1,49 @@
 import { auth } from "@/app/auth";
+import { getMoviesDetails, getWatchLaterList } from "@/lib/data-service";
+import classes from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 async function page() {
   const session = await auth();
+  const moviesList = await getWatchLaterList(session.user.email);
+  const moviesIds = moviesList.map((movie) => movie.movieId);
+
+  const moviesDetails = await getMoviesDetails(moviesIds);
+
+  console.log(moviesList);
 
   return (
     <>
       <h1>Movies you are planning to watch</h1>
+      <div className={classes["movie-list"]}>
+      {moviesDetails.map((movie) => (
+          
+            <Link
+              href={`/search/movie/${movie.id}`}
+              className={classes.card}
+              key={movie.id}
+              // style={{ border: "1px solid #ccc", padding: "1rem" }}
+            >
+              <div className={classes["image-wrapper"]}>
+                <Image
+                  fill
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt={movie.title}
+                  className={classes["card-img"]}
+                />
+              </div>
+              <h2>{movie.title}</h2>
+              <p>
+                <strong>Release Date:</strong> {movie.release_date}
+              </p>
+              <p>
+                <strong>Rating:</strong> {movie.vote_average}
+              </p>
+            </Link>
+          ))}
+        </div>
+      
     </>
   );
 }
